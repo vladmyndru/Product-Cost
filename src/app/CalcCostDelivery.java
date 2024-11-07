f190af4636ed3bf721bfaa99119428ef8930e593
vladmyndru <ladvis62@gmail.com>
2024-11-07 17:56:47 +0200

    Initial commit

---
+++ src/app/CalcCostDelivery.java
@@ -0,0 +1,12 @@
+package app;
+
+public class CalcCostDelivery extends CalcCostBase {
+
+    private final static double deliveryPrice = 5;
+
+    public double calcCost (Product product) {
+        return super.calcCost(product) + deliveryPrice;
+    }
+}
+
+
